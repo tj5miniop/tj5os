b08270ed161098a78d792d0581cfab1efbd5e834
tj5miniop <82961157+tj5miniop@users.noreply.github.com>
2025-06-05 15:55:14 +0100

    hopefully fix build

--- build_files/build.sh
+++ build_files/build.sh
@@ -8,7 +8,7 @@ dnf5 -y upgrade
 
 #Add CachyOS kernel COPR repo and replace the Kernel packages (this will break some stuff which I intend to fix after prototyping)
 dnf5 -y copr enable bieszczaders/kernel-cachyos
-rpm-ostree override remove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra --install kernel-cachyos
+rpm-ostree override remove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra kernel-uki-virt --install kernel-cachyos
 dnf5 -y copr disable bieszczaders/kernel-cachyos
 
 # Add cachy OS addons (modified instructions from COPR page https://copr.fedorainfracloud.org/coprs/bieszczaders/kernel-cachyos-addons/)
