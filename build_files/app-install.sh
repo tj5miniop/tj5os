e6a3c932660e0d3f4a05ef7758ec0d06d48f8311
tj5miniop <82961157+tj5miniop@users.noreply.github.com>
2025-06-08 23:41:14 +0100

    Update app-install.sh

--- build_files/app-install.sh
+++ build_files/app-install.sh
@@ -7,8 +7,8 @@ flatpak update
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 # Install flatpaks - Browser
-flatpak install flathub com.brave.Browser
-flatpak install flathub com.vysp3r.ProtonPlus
-flatpak install flathub com.github.tchx84.Flatseal
-flatpak install flathub com.ranfdev.DistroShelf
-flatpak uninstall --unused
+#flatpak install flathub com.brave.Browser
+#flatpak install flathub com.vysp3r.ProtonPlus
+#flatpak install flathub com.github.tchx84.Flatseal
+#flatpak install flathub com.ranfdev.DistroShelf
+#flatpak uninstall --unused
