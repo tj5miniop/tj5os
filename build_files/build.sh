7ad381935b941ce90bb9717836fe3dce54cc7c03
tj5miniop <82961157+tj5miniop@users.noreply.github.com>
2025-06-08 10:21:47 +0100

    Update build.sh - try install CachyKernel instead

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,17 @@ dnf5 -y install distrobox podman
 
 dnf5 -y clean all 
 
-# NOTICE - Kernel installation will be moved to a separate script/Containerfile
+# NOTICE - Kernel installation will be moved to a separate script/Containerfile - as of 08/06/25 - trying this now in build.sh
+dnf5 -y config-manager addrepo --from-repofile=https://negativo17.org/repos/fedora-steam.repo
+dnf5 -y config-manager addrepo --from-repofile=https://negativo17.org/repos/fedora-rar.repo
+dnf5 -y config-manager setopt "*fedora*".exclude="mesa-* kernel-core-* kernel-modules-* kernel-uki-virt-*"
+# Add SELinux override to install kernel
+setsebool -P domain_kernel_load_modules on
+sudo dnf remove kernel* --allowerasing
+dnf5 -y copr enable bieszczaders/kernel-cachyos
+rpm-ostree override remove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra --install kernel-cachyos
+dnf5 -y copr enable bieszczaders/kernel-cachyos
+dracut -f --regenerate-all
 
 
 
