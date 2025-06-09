dca3f54c017739afa4b1bb97961368d54b16a5ce
tj5miniop <82961157+tj5miniop@users.noreply.github.com>
2025-06-09 12:37:00 +0100

    Cleanup image

--- build_files/cleanup.sh
+++ build_files/cleanup.sh
@@ -5,4 +5,14 @@ set -eoux pipefail
 dnf5 clean all
 rm -rf /tmp/* || true
 
+# Clean /var directory while preserving essential files
+find /var/* -maxdepth 0 -type d \! -name cache -exec rm -fr {} \;
+find /var/cache/* -maxdepth 0 -type d \! -name libdnf5 \! -name rpm-ostree -exec rm -fr {} \;
+
+
+# Restore and setup directories
+mkdir -p /var/tmp
+chmod -R 1777 /var/tmp
+
+
 ostree container commit
