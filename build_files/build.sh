86aae2d44443165bb04144b87861b29245b89543
tj5miniop <82961157+tj5miniop@users.noreply.github.com>
2025-06-09 13:22:11 +0100

    try to fix steam installation

--- build_files/build.sh
+++ build_files/build.sh
@@ -11,8 +11,8 @@ dnf5 -y install distrobox podman
 dnf5 -y clean all 
 
 # NOTICE - Kernel installation will be moved to a separate script/Containerfile - as of 08/06/25 - trying this now in build.sh - Note, kernel installation appears to be working
-dnf5 -y config-manager addrepo --from-repofile=https://negativo17.org/repos/fedora-steam.repo
 dnf5 -y config-manager addrepo --from-repofile=https://negativo17.org/repos/fedora-rar.repo
+dnf5 -y config-manager addrepo --from-repofile=https://negativo17.org/repos/fedora-steam.repo --overwrite
 dnf5 -y config-manager setopt "*fedora*".exclude="mesa-* kernel-core-* kernel-modules-* kernel-uki-virt-*"
 # Add SELinux override to install kernel
 setsebool -P domain_kernel_load_modules on
@@ -32,6 +32,9 @@ dnf5 -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release
 dnf5 -y config-manager setopt fedora-cisco-openh264.enabled=1
 dnf5 -y update --refresh
 
+#Install steam
+dnf5 -y install steam --allowerasing
+
 #Install CachyOS optimisations
 dnf5 -y copr enable bieszczaders/kernel-cachyos-addons
 dnf5 -y update
@@ -39,9 +42,6 @@ dnf5 -y install libcap-ng libcap-ng-devel procps-ng procps-ng-devel
 dnf5 -y install cachyos-settings scx-scheds uksmd --allowerasing
 dnf5 -y copr disable bieszczaders/kernel-cachyos-addons
 dnf5 -y clean all
-
- #Install Steam Package
-dnf5 -y install steam --allowerasing
 
 dnf5 -y copr enable danayer/mesa-git 
 dnf5 -y copr enable danayer/Vulkan-Git
